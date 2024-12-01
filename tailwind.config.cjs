ba885818b1372a15899f4f0d816a3844aa45d2eb
August Schultz-Jørgensen <ausc0001@stud.kea.dk>
2024-12-01 22:08:47 +0100

    Farve og typografi variabler

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,4 +7,39 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'main': ["Manrope"],
+            },
+            colors: {
+                'white': {
+                    white: '#FDFDFD', 
+                },
+                'cream': {
+                    cream: '#FCF9F0',
+                },
+                'black': {
+                    100: '#D4D4D4',
+                    200: '#A6A6A6',
+                    400: '#4D4D4D',
+                    500: '#212121',
+                },
+                'red': {
+                    500: '#ED500E',
+                    700: '#8C2E08',
+                    800: '#602105',
+                },
+                'blue': {
+                    200: '#DAF1FD',
+                },
+                'yellow': {
+                    50: '#FEF7EB',
+                    100: '#FEF2DC',
+                    300: '#FBD592',
+                    500: '#F8BB4D',
+                },
+            },
+        },
+    },
   };
