370073346ad3bad83593c2d9b77e4f5c89661e02
August Schultz-Jørgensen <ausc0001@stud.kea.dk>
2024-12-01 22:17:13 +0100

    Typografi variabler opdateret

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                'main': ["Manrope"],
+                'main': ['"Manrope"'],
             },
             colors: {
                 'white': {
